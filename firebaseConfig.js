fa6af3851d45ba1e8fd4cb88fc166647ebabe79a
Ayush <viridescentyt@gmail.com>
2023-04-19 21:53:34 +0530

    Add additional dependencies and firebase configuration to the application

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA7nR99ST7_UoFQ6lz58em23BvkYzkNu1I",
+  authDomain: "fenced-ba973.firebaseapp.com",
+  projectId: "fenced-ba973",
+  storageBucket: "fenced-ba973.appspot.com",
+  messagingSenderId: "292863358603",
+  appId: "1:292863358603:web:b4f216186f1933649dc68e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ pages/Initial.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Initial = () => {
+  return (
+    <View>
+      <Text>This is Initial Component</Text>
+    </View>
+  );
+};
+
+export default Initial;
